e3b00ed5169833874a7271dad975e44057b477c9
Shane Loretz <sloretz@osrfoundation.org>
2020-09-25 16:50:38 -0700

    Add pose_visual_in_frame test
    Signed-off-by: Shane Loretz <sloretz@osrfoundation.org>

--- test/sdf_pose_tests.cpp
+++ test/sdf_pose_tests.cpp
@@ -188,3 +188,26 @@ TEST(Pose, pose_visual)
   EXPECT_POSE(expected_visual_pose, link->visual->origin);
   EXPECT_POSE(expected_other_pose, link->collision->origin);
 }
+
+TEST(Pose, pose_visual_in_frame)
+{
+  sdf::Errors errors;
+  urdf::ModelInterfaceSharedPtr model = sdformat_urdf::parse(
+    get_file(POSE_VISUAL_IN_FRAME_PATH_TO_SDF), errors);
+  EXPECT_TRUE(errors.empty()) << errors;
+  ASSERT_TRUE(model);
+  ASSERT_EQ("pose_visual_in_frame", model->getName());
+
+  ASSERT_EQ(1u, model->links_.size());
+  urdf::LinkConstSharedPtr link = model->getRoot();
+  ASSERT_NE(nullptr, link);
+
+  const ignition::math::Pose3d frame_pose(0.05, 0.1, 0.2, 0.1, 0.2, 0.3);
+  const ignition::math::Pose3d expected_visual_pose =
+    ignition::math::Pose3d{0.2, 0.4, 0.8, 0.2, 0.3, 0.4} + frame_pose;
+  const ignition::math::Pose3d expected_other_pose{0, 0, 0, 0, 0, 0};
+
+  EXPECT_POSE(expected_other_pose, link->inertial->origin);
+  EXPECT_POSE(expected_visual_pose, link->visual->origin);
+  EXPECT_POSE(expected_other_pose, link->collision->origin);
+}
