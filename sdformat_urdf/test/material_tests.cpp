c816318bbf0f7c52aae90347e6aa3d7593425405
Shane Loretz <sloretz@osrfoundation.org>
2020-10-09 08:51:01 -0700

    Add material_blinn_phong test
    Signed-off-by: Shane Loretz <sloretz@osrfoundation.org>

---
+++ sdformat_urdf/test/material_tests.cpp
@@ -0,0 +1,43 @@
+// Copyright 2020 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+#include <gtest/gtest.h>
+#include <sdf/sdf.hh>
+#include <sdformat_urdf/sdformat_urdf.hpp>
+#include <urdf_model/model.h>
+#include <urdf_model/types.h>
+
+#include "sdf_paths.hpp"
+#include "test_tools.hpp"
+
+TEST(Material, material_blinn_phong)
+{
+  sdf::Errors errors;
+  urdf::ModelInterfaceSharedPtr model = sdformat_urdf::parse(
+    get_file(PATH_TO_SDF_MATERIAL_BLINN_PHONG), errors);
+  EXPECT_TRUE(errors.empty()) << errors;
+  ASSERT_TRUE(model);
+  ASSERT_EQ("material_blinn_phong", model->getName());
+
+  urdf::VisualConstSharedPtr visual = model->getRoot()->visual;
+  ASSERT_NE(nullptr, visual);
+
+  EXPECT_EQ(visual->name, visual->material->name);
+  EXPECT_EQ("", visual->material->texture_filename);
+  EXPECT_FLOAT_EQ(0.3, visual->material->color.r);
+  EXPECT_FLOAT_EQ(0, visual->material->color.g);
+  EXPECT_FLOAT_EQ(0, visual->material->color.b);
+  EXPECT_FLOAT_EQ(1, visual->material->color.a);
+}
